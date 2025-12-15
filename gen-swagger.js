8c21655323ee046c1610126c27a9444343d6174d
paupet7 <114399326+paupet7@users.noreply.github.com>
2025-12-15 10:19:33 +0200

    Add files via upload

---
+++ gen-swagger.js
@@ -0,0 +1,7 @@
+/*const fs = require('fs');
+const swaggerSpec = require('./swagger');
+
+// Write to file
+fs.writeFileSync('./swagger-output.json', JSON.stringify(swaggerSpec, null, 2));
+
+console.log('✅ Swagger JSON generated at ./swagger-output.json');*/
